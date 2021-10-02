f59dbdfe23128b93c91617520fc02e9b5b150759
Felipe Rocha de Lucena - 40259869813 <felipe.lucena@anima.intranet>
2021-10-01 23:27:51 -0300

    using partial types

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,33 +1,9 @@
 import { IsOptional, IsString } from 'class-validator';
-import { getUniqueErrorMessage } from '../../constants/error.constants';
 import { UserRole } from '../../enums/user-role.enum';
-import { IsDocAlreadyInUse } from '../validations/users.doc-constraint';
-import { IsWhatsappAlreadyInUse } from '../validations/users.whatsapp-constraint';
+import { PartialType } from '@nestjs/swagger';
+import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto {
-  @IsOptional()
-  @IsString()
-  name?: string;
-
-  @IsOptional()
-  @IsString()
-  socialName?: string;
-
-  @IsOptional()
-  @IsString()
-  @IsDocAlreadyInUse({ message: getUniqueErrorMessage('doc') })
-  doc?: string;
-
-  @IsOptional()
-  @IsString()
-  @IsWhatsappAlreadyInUse({ message: getUniqueErrorMessage('whatsapp') })
-  whatsapp?: string;
-
-  @IsOptional()
-  @IsString()
-  password?: string;
-
-  //TODO: Validate if role is enum type
+export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsOptional()
   @IsString()
   role?: UserRole;
