0d49ba87d7e41d1997b778d7181bdd6fa27ee7f1
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-12 14:03:12 -0300

    Create Schedules resoucers

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { AddressesModule } from './addresses/addresses.module';
+import { SchedulesModule } from './schedules/schedules.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, AddressesModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, AddressesModule, SchedulesModule],
   controllers: [AppController],
   providers: [AppService],
 })
