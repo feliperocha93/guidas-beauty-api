a80576b3051f0e8c3f187558b317d9c569c9990c
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-10-12 17:02:51 -0300

    created schedule entity and schedule create dto

--- src/schedules/dto/create-schedule.dto.ts
+++ src/schedules/dto/create-schedule.dto.ts
@@ -1,1 +1,40 @@
-export class CreateScheduleDto {}
+import {
+  IsBoolean,
+  IsDate,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateScheduleDto {
+  @IsDate()
+  @IsNotEmpty()
+  schedule: Date;
+
+  @IsInt()
+  @IsNotEmpty()
+  addressId: number;
+
+  @IsBoolean()
+  reserved?: boolean;
+  //if this value to be true, field bookingDate should be required
+
+  @IsOptional()
+  @IsInt()
+  @IsNotEmpty()
+  userId?: number;
+
+  @IsOptional()
+  @IsDate()
+  @IsNotEmpty()
+  bookingDate?: Date;
+  //rules:
+  //this property only can be value if property reserved is be true
+  //this value can not be after schedule property value
+  //if property reserved is changed to false, this value should back to be null
+
+  @IsOptional()
+  @IsBoolean()
+  accomplished?: boolean;
+  //rules: This value only can be true if schedule Date is past
+}
--- src/schedules/entities/schedule.entity.ts
+++ src/schedules/entities/schedule.entity.ts
@@ -1,1 +1,27 @@
-export class Schedule {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Address } from '../../addresses/entities/address.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Schedule {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  schedule: Date;
+
+  @ManyToOne(() => Address, (address) => address.id)
+  addressId: number;
+
+  @Column({ default: false })
+  reserved: boolean;
+
+  @ManyToOne(() => User, (user) => user.id)
+  userId?: number;
+
+  @Column()
+  bookingDate?: Date;
+
+  @Column()
+  accomplished?: boolean;
+}
